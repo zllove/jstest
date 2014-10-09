27972942e39fd231b0d4843555c3ad8006a03476
jikeytang@gmail.com <hong1231>
2014-10-09 17:04:18 +0800

    test

---
+++ frameSource/seajs/js/cat.js
@@ -0,0 +1,20 @@
+(function(root, factory){
+    if(typeof define === 'function' && define.amd){
+        define(['jquery', 'define'], function($, exports){
+            root.cat = factory(root, exports, $);
+        });
+    } else if(typeof exports !== 'undefined') {
+        var $ = require('jquery');
+        factory(root, exports, $);
+    } else {
+        root.cat = factory(root, {}, (root.jQuery || root.$));
+    }
+}(this, function(root, cat, $){
+    $.extend(cat, {
+       init : function(){
+           alert(111);
+       }
+    });
+
+    return cat;
+}));
